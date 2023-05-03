0ae7bc75f05025e510d8552fe69172cbeee84c17
Stefan Koch <74369594+webokoch@users.noreply.github.com>
2023-05-03 20:00:30 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,31 @@
+export const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        emoji: "🍕"
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "🍺",
+        id: 2
+    },
+        {
+        name: "Whisky Sour",
+        ingredients: ["whisky, sugar, lemon, eggwhite"],
+        price: 14,
+        emoji: "🥃",
+        id: 3
+    }
+]
+ 
